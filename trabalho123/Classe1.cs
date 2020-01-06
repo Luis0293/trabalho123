f220b8a557e8fc39c72707a3f9d0083b2400fabf
Luís Guerra <lccpg0293@gmail.com>
2020-01-06 17:39:24 +0000

    Adicionar arquivos de projeto.

---
+++ trabalho123/Classe1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho123
+{
+    class Classe1
+    {
+        private double _n1, _n2; // _a -> Altura ; _l -> Largura
+
+        public string Altura { set => _n1 = Convert.ToDouble(value); }
+        public string Largura { set => _n2 = Convert.ToDouble(value); }
+
+
+
+        public string Multiplicar => (_n1 * _n2).ToString();
+        public string Somar => (_n1 + _n2).ToString();
+        public string Subtrair => (_n1 - _n2).ToString();
+        public string Dividir => (_n1 / _n2).ToString();
+    }
+}
---
+++ trabalho123/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trabalho123
+{
+    public partial class Form1 : Form
+    {
+
+        Classe1 calc = new Classe1();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            calc.Altura = textBox1.Text;
+            calc.Largura = textBox2.Text;
+            if (radioButton1.Checked) label3.Text = calc.Multiplicar;
+            else if (radioButton2.Checked) label3.Text = calc.Dividir;
+            else if (radioButton3.Checked) label3.Text = calc.Somar;
+            else if (radioButton4.Checked) label3.Text = calc.Subtrair;
+        }
+    }
+}
